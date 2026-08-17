8bc0023902d66166ff85320e98884caadddcac67
Lucca Rosa <lucca.rosa@antena1.com.br>
2026-08-17 14:39:36 -0300

    Refatora sistema de armazém aplicando SOLID

---
+++ warehouse/src/main/java/br/com/dio/Box.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static java.math.RoundingMode.CEILING;
+
+public record Box(long amount, LocalDate validate, BigDecimal price) {
+
+    public BigDecimal unitPrice(){
+        return price().divide(new BigDecimal(amount()), CEILING);
+    }
+
+}
+
