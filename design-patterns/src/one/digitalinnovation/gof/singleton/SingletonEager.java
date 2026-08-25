497688f426aa60d7c72479b96c5fb7dcf050960c
Lucca Rosa <lucca.rosa@antena1.com.br>
2026-08-25 12:17:29 -0300

    Implementa exemplos de design patterns

---
+++ design-patterns/src/one/digitalinnovation/gof/singleton/SingletonEager.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof.singleton;
+
+/*
+    Singleton "apressado" - já vai instanciar logo de inicio
+    @author luccarosaa
+ */
+
+public class SingletonEager {
+
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager(){
+        super();
+    }
+
+    public static SingletonEager getInstancia() {
+        return instancia;
+    }
+
+}
